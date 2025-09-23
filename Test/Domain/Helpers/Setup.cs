a4574924316643ce7999efe7ee2810e00fa73c26
vitoriacosmo <vitoriacosmo@outlook.com>
2025-09-23 10:21:38 -0300

    testes de request

---
+++ Test/Domain/Helpers/Setup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MinimalApi.Dominio.Interfaces;
+using Test.Mocks;
+
+namespace Test.Helpers;
+
+public class Setup
+{
+    public const string PORT = "5001";
+    public static TestContext testContext = default!;
+    public static WebApplicationFactory<Program> http = default!;
+    public static HttpClient client = default!;
+
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.testContext = testContext;
+        Setup.http = new WebApplicationFactory<Program>();
+
+        Setup.http = Setup.http.WithWebHostBuilder(builder =>
+        {
+            builder.UseSetting("https_port", Setup.PORT).UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped<IAdministradorServico, AdministradorServicoMock>();
+            });
+
+        });
+
+        Setup.client = Setup.http.CreateClient();
+    }
+
+    public static void ClassCleanup()
+    {
+        Setup.http.Dispose();
+    }
+}
---
+++ Test/Domain/Requests/AdministradorRequestTest.cs
@@ -0,0 +1,58 @@
+﻿using MinimalApi.Dominio.DTOs;
+using MinimalApi.Dominio.Enuns;
+using MinimalApi.Dominio.ModelViews;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Test.Helpers;
+
+namespace Test.Requests;
+
+[TestClass]
+public class AdministradorRequestTest
+{
+    [ClassInitialize]
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.ClassInit(testContext);
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Setup.ClassCleanup();
+    }
+
+    [TestMethod]
+    public async Task TestarGetSetPropriedades()
+    {
+        // Arrange
+        var loginDTO = new LoginDTO
+        {
+            Email = "adm@teste.com",
+            Senha = "123456"
+        };
+
+        var content = new StringContent(JsonSerializer.Serialize(loginDTO), Encoding.UTF8, "Application/json");
+
+        // Act
+        var response = await Setup.client.PostAsync("/administradores/login", content);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        var result = await response.Content.ReadAsStringAsync();
+        var admLogado = JsonSerializer.Deserialize<AdministradorLogado>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(admLogado);
+        Assert.IsFalse(string.IsNullOrEmpty(admLogado?.Email));
+        Assert.IsTrue(Enum.IsDefined(typeof(Perfil), admLogado.Perfil));
+        Assert.IsFalse(string.IsNullOrEmpty(admLogado?.Token));
+
+
+        Console.WriteLine(admLogado?.Token);
+    }
+}
